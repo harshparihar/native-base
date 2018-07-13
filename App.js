63e37995e5b023f6390ed535125805e9a0c845ee
Harshvardhan Parihar <harshvardhan@shoto.com>
2018-07-13 23:51:07 +0530

    List Icon - Settings

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Expo from "expo";
 import { Image } from "react-native";
-import { Container, Header } from "native-base";
-import { Col, Row, Grid } from 'react-native-easy-grid';
+// import { Container, Header, Content, List, ListItem, Text } from "native-base";
+import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch, Button } from 'native-base';
+
 
 
 
@@ -28,10 +29,49 @@ export default class App extends React.Component {
     return (
       <Container>
         <Header />
-          <Grid>
-            <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
-            <Col style={{ backgroundColor: '#00CE9F', height: 200 }}></Col>
-          </Grid>
+        <Content>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#FF9501" }}>
+                <Icon active name="plane" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Airplane Mode</Text>
+            </Body>
+            <Right>
+              <Switch value={false} />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="wifi" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Wi-Fi</Text>
+            </Body>
+            <Right>
+              <Text>GeekyAnts</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem icon>
+            <Left>
+              <Button style={{ backgroundColor: "#007AFF" }}>
+                <Icon active name="bluetooth" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>Bluetooth</Text>
+            </Body>
+            <Right>
+              <Text>On</Text>
+              <Icon active name="arrow-forward" />
+            </Right>
+          </ListItem>
+        </Content>
       </Container>
     );
   }
