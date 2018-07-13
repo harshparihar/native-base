0545bc531ed9a157e4949047b7f3980e2df04daf
Harshvardhan Parihar <harshvardhan@shoto.com>
2018-07-13 21:11:53 +0530

    Checkbox

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Expo from "expo";
 import { Image } from "react-native";
-import { Container, Header, Content, Card, CardItem, Body, View, Text, Button, Icon, Left, Right, Thumbnail } from "native-base";
+import { Container, Header, Content, Card, CardItem, Body, View, Text, Button, Icon, ListItem, CheckBox } from "native-base";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -26,36 +26,24 @@ export default class App extends React.Component {
       <Container>
         <Header />
         <Content>
-          <Card>
-            <CardItem>
-              <Left>
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>GeekyAnts</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image style={{height: 200, width: 300, flex: 1}} source={{uri: "http://www.hdwallpapery.com/static/cache/c1/86/c1869234c08e1ca776656682781bc1b4.jpg"}}/>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent>
-                  <Icon active name="thumbs-up" />
-                  <Text>12 Likes</Text>
-                </Button>
-              </Left>
-              <Body>
-                <Button transparent>
-                  <Icon active name="chatbubbles" />
-                  <Text>4 Comments</Text>
-                </Button>
-              </Body>
-              <Right>
-                <Text>11h ago</Text>
-              </Right>
-            </CardItem>
-          </Card>
+          <ListItem>
+            <CheckBox checked={true} />
+            <Body>
+              <Text>Daily Stand Up</Text>
+            </Body>
+          </ListItem>
+          <ListItem>
+            <CheckBox checked={false} />
+            <Body>
+              <Text>Discussion with Client</Text>
+            </Body>
+          </ListItem>
+          <ListItem>
+            <CheckBox checked={false} color="green"/>
+            <Body>
+              <Text>Finish list Screen</Text>
+            </Body>
+          </ListItem>
         </Content>
       </Container>
     );
