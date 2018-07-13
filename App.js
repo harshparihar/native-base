015b7592ae71e55ff58f9466fa1f9dbecdf0c749
Harshvardhan Parihar <harshvardhan@shoto.com>
2018-07-13 20:06:56 +0530

    Card with image

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Expo from "expo";
-import { Container, Header, Content, Card, CardItem, Body, View, Text, Icon, Right } from "native-base";
+import { Image } from "react-native";
+import { Container, Header, Content, Card, CardItem, Body, View, Text, Button, Icon, Left, Right, Thumbnail } from "native-base";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,13 +28,34 @@ export default class App extends React.Component {
         <Content>
           <Card>
             <CardItem>
-              <Icon active name="logo-googleplus" />
-              <Text>Google Plus</Text>
+              <Left>
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>GeekyAnts</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image style={{height: 200, width: 300, flex: 1}} source={{uri: "http://www.hdwallpapery.com/static/cache/c1/86/c1869234c08e1ca776656682781bc1b4.jpg"}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent>
+                  <Icon active name="thumbs-up" />
+                  <Text>12 Likes</Text>
+                </Button>
+              </Left>
+              <Body>
+                <Button transparent>
+                  <Icon active name="chatbubbles" />
+                  <Text>4 Comments</Text>
+                </Button>
+              </Body>
               <Right>
-                <Icon name="arrow-forward" />
+                <Text>11h ago</Text>
               </Right>
-             </CardItem>
-           </Card>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
