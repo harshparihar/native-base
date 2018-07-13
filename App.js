c641e695a88589de2d38811ae7d3c807aa2eb777
Harshvardhan Parihar <harshvardhan@shoto.com>
2018-07-13 21:48:51 +0530

    FAB - social media sharing options

--- App.js
+++ App.js
@@ -1,40 +1,14 @@
 import React from "react";
 import Expo from "expo";
 import { Image } from "react-native";
-import { Container, Header, Content, DeckSwiper, Card, CardItem, Body, View, Text, Button, Icon, Thumbnail, Left } from "native-base";
+import { Container, Header, Content, DeckSwiper, Card, CardItem, Body, View, Text, Button, Icon, Fab } from "native-base";
 
-const cards = [
-  {
-    text: 'Card One',
-    name: 'One',
-    image: require('./src/assets/images/n1.jpg'),
-  },
-  {
-    text: 'Card Two',
-    name: 'Two',
-    image: require('./src/assets/images/n2.jpg'),
-  },
-  {
-    text: 'Card Three',
-    name: 'Three',
-    image: require('./src/assets/images/n3.jpg'),
-  },
-  {
-    text: 'Card Four',
-    name: 'Four',
-    image: require('./src/assets/images/n4.jpg'),
-  }
-];
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, chosenDate: new Date() };
-    this.setDate = this.setDate.bind(this);
-  }
-
-  setDate(newDate) {
-    this.setState({ chosenDate: newDate });
+    this.state = { loading: true, active: 'true' };
   }
 
   async componentWillMount() {
@@ -53,30 +27,25 @@ export default class App extends React.Component {
     return (
       <Container>
         <Header />
-        <View>
-          <DeckSwiper
-            dataSource={cards}
-            renderItem={item =>
-              <Card style={{ elevation: 3 }}>
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={item.image} />
-                    <Body>
-                      <Text>{item.text}</Text>
-                      <Text note>NativeBase</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem cardBody>
-                  <Image style={{ height: 300, flex: 1 }} source={item.image} />
-                </CardItem>
-                <CardItem>
-                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                  <Text>{item.name}</Text>
-                </CardItem>
-              </Card>
-            }
-          />
+        <View style={{ flex: 1 }}>
+          <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ }}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            <Icon name="share" />
+            <Button style={{ backgroundColor: '#34A34F' }}>
+              <Icon name="logo-whatsapp" />
+            </Button>
+            <Button style={{ backgroundColor: '#3B5998' }}>
+              <Icon name="logo-facebook" />
+            </Button>
+            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+              <Icon name="mail" />
+            </Button>
+          </Fab>
         </View>
       </Container>
     );
