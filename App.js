e108f365bfc40e7b3a2bbc68bca032bb5d8b5da8
Harshvardhan Parihar <harshvardhan@shoto.com>
2018-07-14 16:44:36 +0530

    React Native router flux

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import Expo from "expo";
 import { Image } from "react-native";
 // import { Container, Header, Content, List, ListItem, Text } from "native-base";
-import { Container, Header, Content, List, ListItem, Text, Icon, Left, Body, Right, Switch, Button } from 'native-base';
+import { Container, Header, Content, Picker, Form } from "native-base";
 
+import { Router, Scene } from 'react-native-router-flux';
 
+import PageOne from './src/pageOne';
+import PageTwo from './src/pageTwo';
 
-
-export default class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { loading: true };
@@ -26,53 +28,14 @@ export default class App extends React.Component {
     if (this.state.loading) {
       return <Expo.AppLoading />;
     }
+
     return (
-      <Container>
-        <Header />
-        <Content>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#FF9501" }}>
-                <Icon active name="plane" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>Airplane Mode</Text>
-            </Body>
-            <Right>
-              <Switch value={false} />
-            </Right>
-          </ListItem>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#007AFF" }}>
-                <Icon active name="wifi" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>Wi-Fi</Text>
-            </Body>
-            <Right>
-              <Text>GeekyAnts</Text>
-              <Icon active name="arrow-forward" />
-            </Right>
-          </ListItem>
-          <ListItem icon>
-            <Left>
-              <Button style={{ backgroundColor: "#007AFF" }}>
-                <Icon active name="bluetooth" />
-              </Button>
-            </Left>
-            <Body>
-              <Text>Bluetooth</Text>
-            </Body>
-            <Right>
-              <Text>On</Text>
-              <Icon active name="arrow-forward" />
-            </Right>
-          </ListItem>
-        </Content>
-      </Container>
-    );
+      <Router hideNavBar= "true">
+        <Scene key="root">
+          <Scene key="pageOne" component={PageOne} title="PageOne" initial={true} />
+          <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
+        </Scene>
+      </Router>
+    )
   }
 }
